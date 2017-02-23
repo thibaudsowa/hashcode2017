bf43c3c67cd7ecc36eb65c542d9091863091ed2c
lpetit <loicpeti@gmail.com>
2017-02-23 20:43:28 +0100

    Writer

--- src/main/java/com/monaboys/tools/Writer.java
+++ src/main/java/com/monaboys/tools/Writer.java
@@ -1,23 +1,63 @@
 package com.monaboys.tools;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import com.monaboys.entity.CacheServer;
+import com.monaboys.entity.Video;
 
 /**
  * Created by lpetit on 23/02/2017.
  */
 public class Writer {
     
+    private static final String SEPARATOR = " ";
     
-    public void write(String fileName/*, la chose à serialiser */) {
-        
-        String output = "";
+    public void write(String fileName, Collection<CacheServer> cacheServers) {
+        PrintWriter writer = null;
         try {
-            Files.write(Paths.get(fileName + ".out"), output.getBytes());
-        } catch (IOException e) {
+            writer = new PrintWriter(fileName, "UTF-8");
+            write(writer, cacheServers);
+        } 
+        catch (IOException e) {
             e.printStackTrace();
         }
+        finally {
+            if(writer != null){
+                writer.close();
+            }
+        }
+    }
+    
+    public void write(PrintWriter writer, Collection<CacheServer> cacheServers){
+        writer.println(getLine(cacheServers));
+        for(CacheServer cacheServer : cacheServers){
+            writer.println(getLine(cacheServer));
+        }
+    }
+    
+    private Collection<CacheServer> getNotEmptyCacheServers(Collection<CacheServer> cacheServers){
+        Collection<CacheServer> notEmptyCacheServers = new ArrayList<>();
+        for(CacheServer cacheServer : cacheServers){
+            if(!cacheServer.getVideos().isEmpty()){
+                notEmptyCacheServers.add(cacheServer);
+            }
+        }
+        return notEmptyCacheServers;
+    }
+    
+    private String getLine(Collection<CacheServer> cacheServers){
+        return Integer.toString(cacheServers.size());
+    }
+    
+    private String getLine(CacheServer cacheServer){
+        StringBuilder sb = new StringBuilder(cacheServer.getId());
+        for(Video video : cacheServer.getVideos().values()){
+            sb.append(SEPARATOR).append(video.getId());
+        }
+        return sb.toString();
     }
     
 }
