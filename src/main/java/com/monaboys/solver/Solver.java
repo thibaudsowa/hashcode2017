9a94450804fb352b7159e0abf0b1f4949aadec7f
Guillaume BILLEY <guillaume.billey@inist.fr>
2017-02-23 22:05:49 +0100

    solve

--- src/main/java/com/monaboys/solver/Solver.java
+++ src/main/java/com/monaboys/solver/Solver.java
@@ -1,14 +1,14 @@
 package com.monaboys.solver;
 
-import com.monaboys.entity.Endpoint;
-import com.monaboys.entity.EndpointCacheServer;
+import com.monaboys.entity.CacheServer;
+import com.monaboys.tools.Statement;
 
-import java.util.List;
+import java.util.Set;
 
 /**
  * Created by matti on 23/02/17.
  */
 public interface Solver {
 
-    List<EndpointCacheServer> solve(Endpoint endpoint);
+    Set<CacheServer> solve(Statement statement);
 }
